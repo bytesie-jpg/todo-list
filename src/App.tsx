17a705db4f979f56d99a4fb1fe1723a85e7461b7
Lacey Dallum <laceydallum@gmail.com>
2025-04-13 20:09:27 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoList } from './TodoList';
+import { Todo } from './Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<Array<Todo>>([]);
+    const [todoContent, setTodoContent] = useState<string>('');
+    const idVal = useRef<number>(0);
+
+
+
+    const deleteTodo = (deleteId: number) => {
+        const todosCopy = [...todos].filter((todo) => {
+            return todo.id !== deleteId;
+        })
+        setTodos(todosCopy);
+    }
+
+    const onEdit = (editedTodo: Todo) => {
+        const todosCopy = [...todos].map((todo) => {
+            if (todo.id === editedTodo.id) {
+                return { ...todo, id: editedTodo.id, content: editedTodo.content }
+            }
+            else return todo;
+        })
+        setTodos(todosCopy);
+    }
+
+    return (
+        <div>
+            <label>Create new Todo</label>
+            <input type='text' value={todoContent} onChange={(e) => setTodoContent(e.target.value)} />
+            <button onClick={() => {
+                const todosCopy = [...todos];
+                todosCopy.push({ content: todoContent, id: idVal.current });
+                setTodos(todosCopy);
+                idVal.current++;
+            }}>Submit</button>
+            <TodoList todos={todos} onDelete={deleteTodo} onEdit={onEdit} />
+      </div>
+    );
 }
 
 export default App;
